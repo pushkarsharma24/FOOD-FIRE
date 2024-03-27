58b9e92d3e5f300a932e9e2e0dcee620fc547277
Pushkar Kr Sharma <pushkarkumarsharma2401@gmail.com>
2024-03-26 23:37:35 -0700

    First Commit

---
+++ Components/Footer.js
@@ -0,0 +1,25 @@
+// Footer component for footer section
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <div className="footer">
+      Created By
+      <i className="fa-solid fa-heart"></i>
+      <a
+        href="/"
+        target="_blank"
+        title=""
+      >
+        Pushkar Sharma
+        
+      </a>
+      <i className="fa-solid fa-copyright"></i>
+        {year}
+        <strong>
+          Food<span>Fire</span>
+        </strong>
+    </div>
+  );
+};
+
+export default Footer;
